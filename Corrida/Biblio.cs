5b7918f236f7fa1d3a3edd0c5622c33b127c864e
Theo Quintão <0000900198@senaimgaluno.com.br>
2025-02-10 11:26:08 -0300

    corrida

---
+++ Corrida/Biblio.cs
@@ -0,0 +1,94 @@
+namespace blibio
+{
+    class Corrida
+    {
+        public static void Andar(
+            ref List<Char> Jogador, int Valor1)
+        {
+            int posiçãoDoCarro = Jogador.IndexOf('>');
+            try
+            {
+                Jogador[posiçãoDoCarro+Valor1] = '>';
+                Jogador[posiçãoDoCarro] = '-';
+            }
+            catch{
+                Console.WriteLine("Você venceu!!!");
+                
+            }
+        }
+        public static void TamanhoCorrida(ref List<char> jogador, int valor)
+        {
+            for(int i = 0; i <= valor;i++)
+            {
+                if(i==0)
+                {
+                    jogador.Add('>');
+                }
+                else{
+                   jogador.Add('-'); 
+                }
+            }
+        }
+        public static int Dado()
+        {
+            Random random = new Random{};
+            int j = 50;
+            for(int i=0;i<=10;i++)
+            {
+                Console.Write($"Resultado: {random.Next(1,7)}");
+                Thread.Sleep(200+(i*j));
+                Console.Clear();
+            }
+            int Resultado = random.Next(1,7);
+            Console.WriteLine($"Resultado: {Resultado}");
+            Facilitador.Pausa();
+            return Resultado;
+        }
+        public static void Imprimir(ref List<char> jogador, int valor)
+        {
+            switch(valor)
+            {
+                case 1:
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                break;
+                case 2:
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                break;
+            }
+            foreach(char i in jogador)
+            {
+                Console.Write(i);
+            }
+            Console.ResetColor();
+            Console.WriteLine();
+        }
+    }
+    class Facilitador
+    {
+        public static int ValorInt()
+        {
+            return1:
+            Console.Clear();
+            Console.Write("Tamanho da pista: ");
+            if(!int.TryParse(Console.ReadLine(), out int valor))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Valor invalido!!");
+                Thread.Sleep(1500);
+                Console.ResetColor();
+                goto return1;
+            }
+
+            return valor;
+        }
+        public static void Pausa()
+        {
+            Console.WriteLine();
+            Console.Write("Aperte ENTER para continuar");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Corrida/Program.cs
@@ -0,0 +1,22 @@
+﻿using blibio;
+
+List<char> Jogador1 = new List<char>{};
+List<char> Jogador2 = new List<char>{};
+
+int Tamanho = Facilitador.ValorInt();
+
+Corrida.TamanhoCorrida(ref Jogador1,Tamanho);
+Corrida.TamanhoCorrida(ref Jogador2,Tamanho);
+
+while(true)
+{
+    Console.Clear();
+
+    Corrida.Imprimir(ref Jogador1,1);
+    Corrida.Imprimir(ref Jogador2,2);
+
+    Console.WriteLine("Jogador vermelho jogue o dado!!!!");
+    Facilitador.Pausa();
+    Corrida.Andar(ref Jogador1, Corrida.Dado());
+
+}
